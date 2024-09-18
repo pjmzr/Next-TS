7f8fb89cfdcc0223fb347f0bd16a1d4417777d7d
Pejman Zarepour <ark.royal212@gmail.com>
2024-09-18 08:26:46 +0330

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '1.5rem',
+        lg: '2rem'
+      }
+    }
   },
   plugins: [],
 };
